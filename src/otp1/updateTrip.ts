6c2c4460f9109ebccdf36e8bb8602eb9bffcc674
Kent Andersen <kent.andersen@entur.org>
2020-05-24 23:32:43 +0200

    Added update trip pattern to /v1/trip-patterns/:id in otp1

---
+++ src/otp1/updateTrip.ts
@@ -0,0 +1,244 @@
+import { parseISO, addSeconds, subSeconds, differenceInSeconds, differenceInMinutes } from 'date-fns'
+import createEnturService, { TripPattern, Leg, EstimatedCall } from '@entur/sdk'
+
+import { first, last } from '../utils/array'
+import { isFlexibleLeg, isTransitLeg } from '../utils/leg'
+import { toISOString } from '../utils/time'
+
+const sdk = createEnturService({
+    clientName: 'entur-search',
+    hosts: {
+        journeyPlanner: process.env.TRANSIT_HOST,
+    },
+})
+
+interface UpdatedEstimatedCall {
+    quay: {
+        id: string
+        name: string
+        timezone: string
+    }
+    realtime: boolean
+    expectedArrivalTime: string
+    expectedDepartureTime: string
+    aimedArrivalTime: string
+    aimedDepartureTime: string
+    destinationDisplay: { frontText: string }
+    notices: {
+        id: string
+    }
+}
+interface ServiceJourneyResponse {
+    serviceJourney?: {
+        estimatedCalls?: UpdatedEstimatedCall[]
+    }
+}
+async function getCallsForServiceJourney(id: string, date: string): Promise<UpdatedEstimatedCall[]> {
+    const query = `
+    query($id:String!,$date:Date!) {
+        serviceJourney(id:$id) {
+            estimatedCalls(date:$date) {
+                quay {
+                    id
+                    name
+                    timezone
+                }
+                realtime
+                expectedArrivalTime
+                expectedDepartureTime
+                aimedArrivalTime
+                aimedDepartureTime
+                destinationDisplay {
+                    frontText
+                }
+                notices {
+                    id
+                }
+            }
+        }
+    }
+    `.trim()
+
+    const data = await sdk.queryJourneyPlanner<ServiceJourneyResponse>(query, { id, date })
+
+    if (!data || !data.serviceJourney || !data.serviceJourney.estimatedCalls) {
+        return Promise.reject('No service journey found')
+    }
+
+    return data.serviceJourney.estimatedCalls
+}
+
+function isSameCall(estimatedCall: UpdatedEstimatedCall, quayId: string, destinationFrontText?: string): boolean {
+    const { quay, destinationDisplay } = estimatedCall
+    return destinationFrontText
+        ? quay.id === quayId && destinationDisplay.frontText === destinationFrontText
+        : quay.id === quayId
+}
+
+function findCallByQuayId(
+    estimatedCalls: UpdatedEstimatedCall[],
+    quayId: string,
+    destinationFrontText?: string,
+): UpdatedEstimatedCall | undefined {
+    return estimatedCalls.find((call) => isSameCall(call, quayId, destinationFrontText))
+}
+
+function findCallIndexByQuayId(
+    estimatedCalls: UpdatedEstimatedCall[],
+    quayId: string,
+    destinationFrontText?: string,
+): number {
+    return estimatedCalls.findIndex((call) => isSameCall(call, quayId, destinationFrontText))
+}
+
+function updateEstimatedCall(call: EstimatedCall, updatedCall?: UpdatedEstimatedCall): EstimatedCall {
+    if (!updatedCall) return call
+
+    const { aimedDepartureTime, expectedDepartureTime } = updatedCall
+    if (!aimedDepartureTime && !expectedDepartureTime) return call
+
+    return {
+        ...call,
+        aimedDepartureTime,
+        expectedDepartureTime,
+    }
+}
+interface UpdatedCalls {
+    fromCall: UpdatedEstimatedCall
+    toCall: UpdatedEstimatedCall
+    intermediateCalls: UpdatedEstimatedCall[]
+}
+
+interface LegWithUpdate {
+    leg: Leg
+    updatedCalls?: UpdatedCalls
+}
+async function updateLeg(leg: Leg): Promise<LegWithUpdate> {
+    if (!leg.serviceJourney?.id || !leg.fromEstimatedCall?.date || !leg.toEstimatedCall) {
+        return { leg }
+    }
+
+    const { serviceJourney, fromEstimatedCall, toEstimatedCall } = leg
+    const updatedEstimatedCalls = await getCallsForServiceJourney(serviceJourney.id, fromEstimatedCall.date)
+
+    const fromFrontText = fromEstimatedCall.destinationDisplay?.frontText
+    const fromQuayId = fromEstimatedCall.quay?.id || ''
+    const fromIndex = findCallIndexByQuayId(updatedEstimatedCalls, fromQuayId, fromFrontText)
+    const fromCall = updatedEstimatedCalls[fromIndex]
+    if (!fromCall) return { leg }
+
+    const toFrontText = toEstimatedCall.destinationDisplay?.frontText
+    const toQuayId = toEstimatedCall.quay?.id || ''
+    const toIndex = findCallIndexByQuayId(updatedEstimatedCalls, toQuayId, toFrontText)
+    const toCall = updatedEstimatedCalls[toIndex]
+    if (!toCall) return { leg }
+
+    const intermediateCalls = updatedEstimatedCalls.slice(fromIndex + 1, toIndex - 1)
+
+    return {
+        leg,
+        updatedCalls: { fromCall, toCall, intermediateCalls },
+    }
+}
+
+function updateTransitLeg(leg: Leg, updatedCalls: UpdatedCalls): Leg {
+    if (!updatedCalls) return leg
+    const { fromEstimatedCall, toEstimatedCall, intermediateEstimatedCalls } = leg
+
+    const { fromCall, toCall, intermediateCalls } = updatedCalls
+
+    const { expectedDepartureTime: expectedStartTime } = fromCall
+    const { expectedArrivalTime: expectedEndTime } = toCall
+    const duration = differenceInSeconds(parseISO(expectedEndTime), parseISO(expectedStartTime))
+    const realtime = fromCall.realtime && toCall.realtime && intermediateCalls.every((call) => call.realtime)
+
+    const updatedFromEstimatedCall = fromEstimatedCall && updateEstimatedCall(fromEstimatedCall, fromCall)
+    const updatedToEstimatedCall = toEstimatedCall && updateEstimatedCall(toEstimatedCall, toCall)
+
+    const updatedIntermediateEstimatedCalls = intermediateEstimatedCalls.map((call) => {
+        if (!call.quay || !call.quay.id) return call
+        const frontText = call.destinationDisplay?.frontText
+        const quayId = call.quay.id
+        const updatedCall = findCallByQuayId(intermediateCalls, quayId, frontText)
+        return updateEstimatedCall(call, updatedCall)
+    })
+
+    return {
+        ...leg,
+        duration,
+        realtime,
+        expectedStartTime,
+        expectedEndTime,
+        fromEstimatedCall: updatedFromEstimatedCall,
+        toEstimatedCall: updatedToEstimatedCall,
+        intermediateEstimatedCalls: updatedIntermediateEstimatedCalls,
+    }
+}
+
+function updateNonTransitLeg(leg: Leg, next?: LegWithUpdate, prev?: LegWithUpdate): Leg {
+    const { duration = 0 } = leg
+
+    if (next) {
+        const { updatedCalls } = next
+        if (!updatedCalls) return leg
+
+        const { fromCall } = updatedCalls
+        const { expectedArrivalTime: expectedEndTime, quay } = fromCall
+        const { timezone: timeZone } = quay
+
+        const expectedStartTime = toISOString(subSeconds(parseISO(expectedEndTime), duration), { timeZone })
+        return { ...leg, expectedStartTime, expectedEndTime }
+    } else if (prev) {
+        const { updatedCalls } = prev
+        if (!updatedCalls) return leg
+
+        const { toCall } = updatedCalls
+        const { expectedDepartureTime: expectedStartTime, quay } = toCall
+        const { timezone: timeZone } = quay
+
+        const expectedEndTime = toISOString(addSeconds(parseISO(expectedStartTime), duration), { timeZone })
+        return { ...leg, expectedStartTime, expectedEndTime }
+    }
+
+    return leg
+}
+
+function mergeLegsWithUpdateInfo(legsWithUpdate: LegWithUpdate[]): Leg[] {
+    return legsWithUpdate.map(({ leg, updatedCalls }, index) => {
+        if (!isTransitLeg(leg)) {
+            const next = legsWithUpdate[index + 1]
+            const prev = legsWithUpdate[index - 1]
+            return updateNonTransitLeg(leg, next, prev)
+        }
+        if (!updatedCalls) return leg
+        return updateTransitLeg(leg, updatedCalls)
+    })
+}
+
+export async function updateTripPattern(tripPattern: TripPattern): Promise<TripPattern> {
+    if (tripPattern.legs.some(isFlexibleLeg)) return tripPattern
+
+    const { legs, startTime, endTime, duration } = tripPattern
+
+    const legsWithUpdateInfo = await Promise.all(legs.map((leg) => updateLeg(leg).catch(() => ({ leg }))))
+    const updatedLegs = mergeLegsWithUpdateInfo(legsWithUpdateInfo)
+
+    const { expectedStartTime: updatedStartTime = startTime } = first(updatedLegs) || {}
+    const { expectedEndTime: updatedEndTime = endTime } = last(updatedLegs) || {}
+    const updatedDuration =
+        updatedStartTime && updatedEndTime ? differenceInSeconds(parseISO(endTime), parseISO(startTime)) : duration
+
+    return {
+        ...tripPattern,
+        startTime: updatedStartTime,
+        endTime: updatedEndTime,
+        duration: updatedDuration,
+        legs: updatedLegs,
+    }
+}
+
+export function getExpires(tripPattern: TripPattern): Date {
+    const now = new Date()
+    const startTime = parseISO(tripPattern.startTime)
+    return differenceInMinutes(startTime, now) > 60 ? addSeconds(now, 30) : startTime
+}
--- src/utils/time.ts
+++ src/utils/time.ts
@@ -1,8 +1,30 @@
 import { findTimeZone, getZonedTime, getUTCOffset } from 'timezone-support'
 
+function pad(num = 0): string {
+    return String(num).padStart(2, '0')
+}
+
+// Lifted from timezone-support/dist/parse-format
+function formatOffset(offset = 0): string {
+    if (!offset) return 'Z'
+    const sign = offset < 0 ? '+' : '-'
+    const absOffset = Math.abs(offset)
+    const hours = Math.floor(absOffset / 60)
+    const minutes = Math.floor(absOffset % 60)
+    return `${sign}${pad(hours)}:${pad(minutes)}`
+}
+
 interface Options {
     timeZone?: string
 }
+export function toISOString(date: Date, options: Options): string {
+    if (!options.timeZone) return date.toISOString()
+    const timeZone = findTimeZone(options.timeZone)
+    const { year, month, day, hours, minutes, seconds, zone } = getZonedTime(date, timeZone)
+    const offset = zone?.offset
+    return `${year}-${pad(month)}-${pad(day)}T${pad(hours)}:${pad(minutes)}:${pad(seconds)}${formatOffset(offset)}`
+}
+
 // Lifted from date-fns-timezone
 export function convertToTimeZone(date: Date, options: Options): Date {
     if (!options.timeZone) return date
