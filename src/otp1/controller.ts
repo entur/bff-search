dfa5df3ea2ad1b29a5cb4cc66296c9d76abb3309
Mats Byrkjeland <mats.byrkjeland@entur.org>
2020-06-11 16:28:43 +0200

    Fix check for if should taxi search be done

--- src/otp1/controller.ts
+++ src/otp1/controller.ts
@@ -64,7 +64,7 @@ export async function searchTransitWithTaxi(
         TripPattern[],
         TransitTripPatterns | undefined,
     ] = await Promise.all([
-        tripPatterns.length ? searchTaxiFrontBack(params, extraHeaders) : [],
+        !tripPatterns.length ? searchTaxiFrontBack(params, extraHeaders) : [],
         firstTransitResults.nextSearchParams
             ? searchTransit(
                   firstTransitResults.nextSearchParams,
