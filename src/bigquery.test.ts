b688064b6ef3294f6d85200178fc81f83e336a59
Mats Byrkjeland <mats.byrkjeland@entur.org>
2021-02-03 16:12:39 +0100

    Add buildInsertQuery for simpler SQL inserts

---
+++ src/bigquery.test.ts
@@ -0,0 +1,37 @@
+import { buildInsertQuery } from './bigquery'
+
+describe('buildInsertQuery', () => {
+    it('should stringify strings', () => {
+        expect(buildInsertQuery('cheeses', { cheese: 'camembert' })).toBe(
+            `INSERT INTO \`cheeses\` (cheese) VALUES ("camembert")`,
+        )
+    })
+    it('should not stringify numbers', () => {
+        expect(buildInsertQuery('cheeses', { quantity: 42 })).toBe(
+            `INSERT INTO \`cheeses\` (quantity) VALUES (42)`,
+        )
+    })
+    it('should not stringify booleans', () => {
+        expect(buildInsertQuery('cheeses', { isGood: true })).toBe(
+            `INSERT INTO \`cheeses\` (isGood) VALUES (true)`,
+        )
+    })
+    it('should join multiple fields with comma', () => {
+        expect(
+            buildInsertQuery('cheeses', { name: 'camembert', quantity: 42 }),
+        ).toBe(
+            `INSERT INTO \`cheeses\` (name, quantity) VALUES ("camembert", 42)`,
+        )
+    })
+    it('should treat undefined values as missing value', () => {
+        expect(buildInsertQuery('cheeses', { nothing: undefined })).toBe(
+            `INSERT INTO \`cheeses\` (nothing) VALUES ()`,
+        )
+        expect(
+            buildInsertQuery('cheeses', { name: 'brie', nothing: undefined }),
+        ).toBe(`INSERT INTO \`cheeses\` (name, nothing) VALUES ("brie",)`)
+    })
+    it('should throw an error if no data', () => {
+        expect(() => buildInsertQuery('cheeses', {})).toThrowError(/empty data/)
+    })
+})
---
+++ src/config.ts
@@ -0,0 +1,13 @@
+/**
+ * This is a dummy file and will be rewritten during build and filled with real values.
+ * Values are cast to string so that TypeScript does not treat them as the empty string literal.
+ */
+export const ENVIRONMENT = '' as string
+export const TRANSIT_HOST = '' as string
+export const NON_TRANSIT_HOST = '' as string
+export const TRANSIT_HOST_OTP2 = '' as string
+export const NON_TRANSIT_HOST_OTP2 = '' as string
+export const REDIS_HOST = '' as string
+export const REDIS_PORT = '' as string
+export const PARTNER_AUDIENCE = '' as string
+export const PARTNER_HOST = '' as string
