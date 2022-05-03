7818e3b6f177ff3b2eee9ef87ab03a345fd793bf
Carl Joachim Otvik <carl.joachim.otvik@entur.org>
2022-05-03 10:16:15 +0200

    Downgraded Redis to 3.1.2

--- src/cache.ts
+++ src/cache.ts
@@ -1,4 +1,5 @@
-import { createClient } from 'redis'
+import redis from 'redis'
+import { promisify } from 'util'
 
 import logger from './logger'
 import { REDIS_HOST, REDIS_PORT } from './config'
@@ -8,9 +9,13 @@ const PROD = process.env.NODE_ENV === 'production'
 const host = PROD ? REDIS_HOST : 'localhost'
 const port = PROD ? Number(REDIS_PORT) : 6379
 
-const client = createClient({ url: `redis://${host}:${port}` })
+const client = redis.createClient(port, host)
 
 client.on('error', (err) => logger.error('REDIS ERROR:', err))
+
+const hgetall = promisify(client.hgetall).bind(client)
+const hset = promisify(client.hset).bind(client)
+const expire = promisify(client.expire).bind(client)
 
 const DEFAULT_EXPIRE = 30 * 60 // 30 minutes
 
@@ -20,20 +25,20 @@ export async function set(
     expireInSeconds: number = DEFAULT_EXPIRE,
 ): Promise<void> {
     logger.debug(`Cache set ${key}`)
-    await client.HSET(key, 'data', JSON.stringify(value))
-    await client.expire(key, expireInSeconds)
+    await hset([key, 'data', JSON.stringify(value)])
+    await expire(key, expireInSeconds)
 }
 
 export async function get<T>(
     key: string,
     expireInSeconds: number = DEFAULT_EXPIRE,
 ): Promise<T | null> {
-    const entry = await client.HGETALL(key)
+    const entry = await hgetall(key)
 
     if (entry === null || entry.data === undefined) {
         return null
     }
-    await client.expire(key, expireInSeconds)
+    await expire(key, expireInSeconds)
 
     return JSON.parse(entry.data)
 }
