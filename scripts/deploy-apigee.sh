915534232b8a457b6d24f051e5a0ca83f994f3e0
Carl Joachim Otvik <carl.joachim.otvik@entur.org>
2022-05-04 14:19:09 +0200

    Updated Apigee deploy setup

--- scripts/deploy-apigee.sh
+++ scripts/deploy-apigee.sh
@@ -11,26 +11,31 @@ function deploy {
     fi
 
     read -rp " 😺 Apigee user: " APIGEEUSER
+    echo "🔑 Get Temporary Apigee password from url https://entur-norway.login.apigee.com/passcode  - log in with SSO"
     read -rsp " 🔑 Apigee password: " APIGEEPASSWORD
-    echo
+
+    APIGEETOKEN=$(curl -H "Content-Type: application/x-www-form-urlencoded;charset=utf-8" -H "accept: application/json;charset=utf-8" -H "Authorization: Basic ZWRnZWNsaTplZGdlY2xpc2VjcmV0" -X POST https://entur-norway.login.apigee.com/oauth/token -s -d "grant_type=password&response_type=token&passcode=${APIGEEPASSWORD}" | jq -r '.access_token')
+    echo -e "Token er satt til $APIGEETOKEN"
 
     if ! command_exists jq; then
+        echo "Brew installing jq..."
         brew install jq
     fi
 
     if [[ "$ENV" == "dev" ]]; then
         echo " 📝 Deploying new revision to Apigee dev ..."
-        apigeetool deployproxy -V -o entur -e dev -n client-search -d api/client-search -u $APIGEEUSER -p $APIGEEPASSWORD
+        apigeetool deployproxy -V -o entur -e dev -n client-search -d api/client-search -t $APIGEETOKEN
     fi
 
-    APIGEEREVISION=$(apigeetool listdeployments -u $APIGEEUSER -p $APIGEEPASSWORD -o entur -n client-search -j | jq '.deployments[] | select(.environment |contains("dev")) |.revision')
+    echo " SETTING APIGEE REVISION"
+    APIGEEREVISION=apigeetool listdeployments -V -u $APIGEEUSER -t $APIGEETOKEN -o entur -n client-search -j | jq '.deployments[] | select(.environment |contains("dev")) |.revision'
 
     if [[ "$ENV" == "staging" ]]; then
         echo " 📝 Deploying revision $APIGEEREVISION to Apigee stage ..."
-        apigeetool deployExistingRevision -V -u $APIGEEUSER -p $APIGEEPASSWORD -o entur -e stage -n client-search -r $APIGEEREVISION
+        apigeetool deployExistingRevision -V -t $APIGEETOKEN -o entur -e stage -n client-search -r $APIGEEREVISION 
     elif [[ "$ENV" == "prod" ]]; then
         echo " 📝 Deploying revision $APIGEEREVISION to Apigee prod ..."
-        apigeetool deployExistingRevision -V -u $APIGEEUSER -p $APIGEEPASSWORD -o entur -e prod -n client-search -r $APIGEEREVISION
+        apigeetool deployExistingRevision -V -t $APIGEETOKEN -o entur -e prod -n client-search -r $APIGEEREVISION
     fi
 
     echo -e "\n 🎉 Revision $APIGEEREVISION successfully deployed to $ENV!"
@@ -54,7 +59,7 @@ function slack_message {
 
     curl -X POST \
         --data-urlencode "payload={\"channel\": \"#team-app-build\", \"username\": \"BFF Search endpoints deployed to Apigee $ENV\", \"text\": \"\`$USER\` deployed version *$VERSION* of *BFF Search* endpoints to Apigee :$ENV: from branch \`$BRANCH\` (\`$COMMIT\`)\", \"icon_emoji\": \":mag:\"}" \
-        https://hooks.slack.com/services/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
+        https://hooks.slack.com/services/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
 }
 
 ENV_ARGS="${@:-dev}"
