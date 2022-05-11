ff96f48b333f8fb3937cc5fce6af00483b8d5f7c
Carl Joachim Otvik <carl.joachim.otvik@entur.org>
2022-05-11 11:51:28 +0200

    Moved to function

--- src/routes/v1/trip-patterns.ts
+++ src/routes/v1/trip-patterns.ts
@@ -24,6 +24,7 @@ import {
     SearchParams,
     TripPattern,
     TripPatternParsed,
+    Leg,
 } from '../../types'
 
 import { getAlternativeTripPatterns } from '../../logic/otp1'
@@ -164,6 +165,45 @@ router.post<'/replace-leg/:id', { id: string }>(
     },
 )
 
+function getExpectedStartTime(newLegs: Leg[]): string {
+    let expectedStartTime = ''
+
+    if (newLegs[0]?.id === null && newLegs[1]?.expectedStartTime) {
+        expectedStartTime = toISOString(
+            subSeconds(
+                parseISO(newLegs[1].expectedStartTime),
+                newLegs[0].duration,
+            ),
+            { timeZone: 'Europe/Oslo' },
+        )
+    } else {
+        expectedStartTime = newLegs[0]?.expectedStartTime || ''
+    }
+    return expectedStartTime
+}
+
+function getExpectedEndTime(newLegs: Leg[]): string {
+    let expectedEndTime = ''
+    if (
+        newLegs.length > 1 &&
+        newLegs[newLegs.length - 1]?.id === null &&
+        newLegs[newLegs.length - 2]?.expectedEndTime
+    ) {
+        const lastTransitExpectedEnd =
+            newLegs[newLegs.length - 2]?.expectedEndTime || ''
+        const lastFootLegDuration = newLegs[newLegs.length - 1]?.duration || 0
+
+        expectedEndTime = toISOString(
+            addSeconds(parseISO(lastTransitExpectedEnd), lastFootLegDuration),
+            { timeZone: 'Europe/Oslo' },
+        )
+    } else {
+        expectedEndTime = newLegs[newLegs.length - 1]?.expectedEndTime || ''
+    }
+
+    return expectedEndTime
+}
+
 router.post<'/replace-tripPattern', { id: string }>(
     '/replace-tripPattern',
     async (req, res, next) => {
@@ -184,42 +224,9 @@ router.post<'/replace-tripPattern', { id: string }>(
                 })
 
                 // If first Leg is foot we have to do some manual modification to the Tripattern
-                let expectedStartTime = ''
-                if (newLegs[0]?.id === null && newLegs[1]?.expectedStartTime) {
-                    expectedStartTime = toISOString(
-                        subSeconds(
-                            parseISO(newLegs[1]?.expectedStartTime),
-                            newLegs[0].duration,
-                        ),
-                        { timeZone: 'Europe/Oslo' },
-                    )
-                } else {
-                    expectedStartTime = newLegs[0]?.expectedStartTime || ''
-                }
-
+                const expectedStartTime = getExpectedStartTime(newLegs)
                 // If last Leg is foot we have to do some manual modification to the Tripattern
-                let expectedEndTime = ''
-                if (
-                    newLegs.length > 1 &&
-                    newLegs[newLegs.length - 1]?.id === null &&
-                    newLegs[newLegs.length - 2]?.expectedEndTime
-                ) {
-                    const lastTransitExpectedEnd =
-                        newLegs[newLegs.length - 2]?.expectedEndTime || ''
-                    const lastFootLegDuration =
-                        newLegs[newLegs.length - 1]?.duration || 0
-
-                    expectedEndTime = toISOString(
-                        addSeconds(
-                            parseISO(lastTransitExpectedEnd),
-                            lastFootLegDuration,
-                        ),
-                        { timeZone: 'Europe/Oslo' },
-                    )
-                } else {
-                    expectedEndTime =
-                        newLegs[newLegs.length - 1]?.expectedEndTime || ''
-                }
+                const expectedEndTime = getExpectedEndTime(newLegs)
 
                 let duration = 0
                 if (expectedStartTime && expectedEndTime) {
