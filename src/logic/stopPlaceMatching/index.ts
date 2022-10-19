c7a36bd376effc98d40fb49db5712364de06df98
Joakim Tysseng <joakim.tysseng@entur.org>
2022-10-19 10:04:20 +0200

    Updated statistics generation

--- src/logic/stopPlaceMatching/index.ts
+++ src/logic/stopPlaceMatching/index.ts
@@ -4,46 +4,64 @@ import { getNearestStops } from '../geocoder'
 import { v4 as uuid } from 'uuid'
 import { SearchParams, TripPatternParsed } from '../../types'
 
-const maxDistanceMeters = 30
-const instanceId = uuid()
-const logInterval = 1000 * 60
+const MAX_DISTANCE_METERS = 30
+const LOG_INTERVAL = 1000 * 60
+const INSTANCE_ID = uuid()
 
-let matchTries = 0
+// statistics counters
+let totalChecks = 0
+
+let stopPlacesWithoutCoordinates = 0
+
+// searches starting within 30m of the user's current position
+let shouldHaveFound = 0
 
 // closest stopPlace and within 30m
 let correctlyFound = 0
 
-// real first stop place is within maxDistanceMeters of GPS position
-let missedButShouldHaveBeenFound = 0
+// real first stop place is within maxDistanceMeters of GPS position but wasn't found as any nearby stop
+let missedCompletely = 0
 
 // real first stop place is > maxDistanceMeters away
 let tooFarAway = 0
 
 // remember that 0 here is the number of cases with only 1 nearby stop etc
 const nearbyCount: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+// ideally we want to find everything at index 0.
 const foundAtIndex: number[] = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+// distance to correct stop place in groups of 10m (0-10, 10-20 etc)
 const foundAtDistance: number[] = [0, 0, 0, 0, 0, 0]
+
 const startTime = Date.now()
-const lastLogTime = Date.now()
+let lastLogTime = Date.now()
 
 // This is a fragile way of detecting that the user has searched for 'your position' but currently it
 // is the only way of knowing.
 const myLocationTexts = ['Posisjonen din', 'Your location']
 
 const logNearbyStatistics = (): void => {
-    if (Date.now() - lastLogTime > logInterval) {
-        const hitRate = Math.round((1000 * correctlyFound) / matchTries) / 10
+    if (Date.now() - lastLogTime > LOG_INTERVAL) {
+        lastLogTime = Date.now()
+        const hitRate =
+            Math.round((1000 * correctlyFound) / shouldHaveFound) / 10
         logger.info(
-            `(id: ${instanceId}) Nearby-matching: Hit rate ${hitRate}. Expand for statistics`,
+            `(id: ${INSTANCE_ID}) Nearby-matching: Hit rate ${hitRate}. Expand for statistics`,
             {
-                instanceId,
+                instanceId: INSTANCE_ID,
                 runningTime: `${Math.round((lastLogTime - startTime) / 1000)}s`,
-                matchTries,
-                correctlyFound,
-                missedButShouldHaveBeenFound,
+                totalChecks,
+                hitRate: {
+                    hitRate,
+                    shouldHaveFound,
+                    correctlyFound,
+                    missedCompletely, // missed even when looking at +20m
+                },
                 tooFarAway,
                 foundAtIndex,
                 foundAtDistance,
+                stopPlacesWithoutCoordinates,
                 distancesFromUserGPS: [
                     ` 0 - 10m: ${foundAtDistance[0]}`,
                     `10 - 20m: ${foundAtDistance[1]}`,
@@ -81,12 +99,8 @@ export const runStopPlaceMatching = async (
 
         if (isApp && isMyLocation && tripPatterns.length !== 0 && lon && lat) {
             // We add 20m to help us gather statistics about the best 'near' distance
-            const maxDistKm = maxDistanceMeters / 100 + 0.02
+            const maxDistKm = MAX_DISTANCE_METERS / 100 + 0.02
             const nearestStops = await getNearestStops(lat, lon, maxDistKm)
-
-            logger.info(`Found ${nearestStops.length} stops nearby`, {
-                nearestStops,
-            })
 
             if (nearestStops.length === 0) {
                 // No stops found within 30 meters, search from current 'my location'
@@ -114,7 +128,25 @@ export const runStopPlaceMatching = async (
                 return
             }
 
-            // When we get as far as this, we know we are able to compare an intitial stopPlace with stops found nearby
+            // When we get as far as this, we know we are able to compare an initial stopPlace with stops found nearby
+
+            const firstStopPlace =
+                firstLegWithStopPlace.fromPlace.quay?.stopPlace
+            if (!firstStopPlace?.latitude || !firstStopPlace.longitude) {
+                // not possible to do statistics so abort.
+                stopPlacesWithoutCoordinates++
+                return
+            }
+
+            // Distance to first stop found in search result
+            const distanceToFirstStop = getDistanceInMeters(
+                lat,
+                lon,
+                firstStopPlace.latitude,
+                firstStopPlace.longitude,
+            )
+
+            if (distanceToFirstStop < maxDistKm) shouldHaveFound++
 
             const firstStopIndex = nearestStops.findIndex((nearbyStop) => {
                 return nearbyStop.properties.id === firstStopPlaceId
@@ -123,9 +155,11 @@ export const runStopPlaceMatching = async (
             if (firstStopIndex > -1) {
                 foundAtIndex[firstStopIndex]++
 
+                /*
                 const distanceToFirstStop =
                     nearestStops[firstStopIndex]?.properties.distance
                 if (!distanceToFirstStop) return
+                 */
 
                 // in 10 mtrs
                 const distanceToFirstStopRounded = Math.ceil(
@@ -133,32 +167,27 @@ export const runStopPlaceMatching = async (
                 )
                 foundAtDistance[distanceToFirstStopRounded]++
 
-                if (distanceToFirstStop < maxDistanceMeters) correctlyFound++
+                if (
+                    distanceToFirstStop <= MAX_DISTANCE_METERS &&
+                    firstStopIndex === 0
+                )
+                    correctlyFound++
             } else {
-                const firstStopPlace =
-                    firstLegWithStopPlace.fromPlace.quay?.stopPlace
-                if (firstStopPlace?.latitude && firstStopPlace.longitude) {
-                    const distanceToFirstStop = getDistanceInMeters(
-                        lat,
-                        lon,
-                        firstStopPlace.latitude,
-                        firstStopPlace.longitude,
-                    )
-                    if (distanceToFirstStop < maxDistanceMeters) {
-                        missedButShouldHaveBeenFound++
-                    } else {
-                        tooFarAway++
-                    }
-                    logger.info(
-                        `First stop is not part of nearby stops, real distance is ${distanceToFirstStop}`,
-                        {
-                            firstLegWithStopPlace,
-                            nearestStops,
-                        },
-                    )
+                if (distanceToFirstStop <= MAX_DISTANCE_METERS) {
+                    // If distance is within max distance we SHOULD have found it
+                    missedCompletely++
+                } else {
+                    tooFarAway++
                 }
+                logger.info(
+                    `First stop is not part of nearby stops, real distance is ${distanceToFirstStop}`,
+                    {
+                        firstLegWithStopPlace,
+                        nearestStops,
+                    },
+                )
             }
-            matchTries++
+            totalChecks++
 
             logNearbyStatistics()
         }
