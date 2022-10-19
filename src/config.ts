9a2b05a88e16c65bb36a5b4099f2b651ad791552
Kristoffer Severinsen <kristoffer.severinsen@entur.org>
2022-10-19 08:55:36 +0200

    remove unused otp1 functions - remove unused config - clean up .env files

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,6 @@
  * Values are cast to string so that TypeScript does not treat them as the empty string literal.
  */
 export const ENVIRONMENT = '' as string
-export const TRANSIT_HOST = '' as string
 export const TRANSIT_HOST_OTP2 = '' as string
 export const PARTNER_AUDIENCE = '' as string
 export const PARTNER_HOST = '' as string
