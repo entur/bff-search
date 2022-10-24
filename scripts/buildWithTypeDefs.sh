fd81446fd04ce940b3c6636598494159008b85a3
tysseng <joakim.tysseng@entur.org>
2022-10-24 10:09:57 +0200

    Separated generation of types from normal build cycle (#180)

--- scripts/buildWithTypeDefs.sh
+++ scripts/buildWithTypeDefs.sh
@@ -6,8 +6,14 @@ if [ $# -eq 0 ]; then
     exit 1
 fi
 
+echo "Clearing any previous build files"
+rm -rf typeDeclarations
+
 echo "Compiling TypeScript"
-tsc -p tsconfig.json
+tsc -p tsconfig.json --declaration --declarationDir "typeDeclarations"
+
+echo "Packaging the type declarations into an archive file"
+tar -czf "typeDeclarations.tar.gz" typeDeclarations/
 
 echo "Populating environment variables for $1"
 npm run populate-env-vars "$1"
