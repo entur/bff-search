e7a32263cee28c23e9d80046c094d3041b528f4e
Carl Joachim Otvik <carl.joachim.otvik@entur.org>
2022-10-28 08:38:39 +0200

    moved variables

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
  * This is a dummy file and will be rewritten during build and filled with real values.
  * Values are cast to string so that TypeScript does not treat them as the empty string literal.
  */
-export const ENVIRONMENT = '' as string
-export const TRANSIT_HOST_OTP2 = '' as string
-export const PARTNER_AUDIENCE = '' as string
-export const PARTNER_HOST = '' as string
+export const ENVIRONMENT = process.env.ENVIRONMENT as string
+export const TRANSIT_HOST_OTP2 = process.env.TRANSIT_HOST_OTP2 as string
+export const PARTNER_AUDIENCE = process.env.PARTNER_AUDIENCE as string
+export const PARTNER_HOST = process.env.PARTNER_HOST as string
