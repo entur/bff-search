240d0e8746aada02ee1da07e166e38ade300af00
Carl Joachim Otvik <carl.joachim.otvik@entur.org>
2022-10-31 12:56:12 +0100

    added nordic-dev and terraform

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,3 @@
-/**
- * This is a dummy file and will be rewritten during build and filled with real values.
- * Values are cast to string so that TypeScript does not treat them as the empty string literal.
- */
 export const ENVIRONMENT = process.env.ENVIRONMENT as string
 export const TRANSIT_HOST_OTP2 = process.env.TRANSIT_HOST_OTP2 as string
 export const PARTNER_AUDIENCE = process.env.PARTNER_AUDIENCE as string
