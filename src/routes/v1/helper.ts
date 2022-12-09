a70d3c39cc19f79b8dd8ce45c8f7d05b620fad72
Carl Joachim Otvik <carl.joachim.otvik@entur.org>
2022-12-09 13:15:04 +0100

    Using entur-search as client name from bff-search

--- src/routes/v1/helper.ts
+++ src/routes/v1/helper.ts
@@ -4,10 +4,8 @@ import { clean } from '../../utils/object'
 import { ExtraHeaders } from '../../types'
 
 export function getHeadersFromClient(req: Request): ExtraHeaders {
-    const clientName = req.get('ET-Client-Name')
-
     return clean({
         'X-Correlation-Id': req.get('X-Correlation-Id'),
-        'ET-Client-Name': clientName ? `${clientName}-bff` : 'entur-search',
+        'ET-Client-Name': 'entur-search',
     })
 }
