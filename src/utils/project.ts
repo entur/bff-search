4aef64969be45ee065869a68e2ca239d1ded9627
Joakim Tysseng <joakim.tysseng@entur.org>
2023-11-07 13:54:41 +0100

    Changing config to new environments

--- src/utils/project.ts
+++ src/utils/project.ts
@@ -1,11 +1,13 @@
 import { ENVIRONMENT } from '../config'
 
 export const getProjectId = (): string => {
-    if (ENVIRONMENT === 'nordic-dev') {
-        return 'ent-clients-nordic-dev'
-    } else if (ENVIRONMENT === 'terraform') {
-        return 'ent-selvbet-terraform-dev'
+    if (ENVIRONMENT === 'staging') {
+        return 'ent-enturapp-tst'
+    } else if (ENVIRONMENT === 'beta') {
+        return 'ent-enturbeta-prd'
+    } else if (ENVIRONMENT === 'prod') {
+        return 'ent-enturapp-prd'
     } else {
-        return `entur-${ENVIRONMENT}`
+        return 'ent-enturapp-dev'
     }
 }
