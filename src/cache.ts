62195b69c05bf729c11ba1e01af786d135c68621
Joakim Tysseng <joakim.tysseng@entur.org>
2024-09-18 13:21:35 +0200

    Removed stuff related to Redis v4 and old way of getting config

--- src/cache.ts
+++ src/cache.ts
@@ -1,9 +1,6 @@
 import { createClient } from 'redis'
-import { Storage } from '@google-cloud/storage'
 
 import logger from './logger'
-import { getProjectId } from './utils/project'
-import semver from 'semver/preload'
 import { getSecret } from './secrets'
 
 const PROD = process.env.NODE_ENV === 'production'
@@ -23,52 +20,15 @@ interface Config {
 }
 
 async function getRedisConfig(): Promise<Config> {
-    try {
-        const redisPassword = await getSecret('REDIS_PASSWORD')
-        const redisHost = await getSecret('REDIS_HOST')
-        const redisPort = await getSecret('REDIS_PORT')
-        if (redisPassword && redisPassword !== '') {
-            logger.info('Loaded redis config from secrets')
-            return {
-                redisHost,
-                redisPort: Number(redisPort),
-                redisPassword,
-            }
-        }
-    } catch {
-        // fall back to 'old' config
+    const redisPassword = await getSecret('REDIS_PASSWORD')
+    const redisHost = await getSecret('REDIS_HOST')
+    const redisPort = await getSecret('REDIS_PORT')
+    logger.info('Loaded redis config from secrets')
+    return {
+        redisHost,
+        redisPort: Number(redisPort),
+        redisPassword,
     }
-
-    // Redis ip and port are stored in a cloud storage bucket when
-    // environment is terraformed (or set manually if terraform is not used)
-    const storage = new Storage()
-    const bucket = storage.bucket(`gs://${getProjectId()}-bff-search-config`)
-
-    const file = await bucket.file('config.json').download()
-    const content = file[0].toString()
-
-    const parsed = JSON.parse(content) as Config
-    if (!parsed.redisPort || !parsed.redisHost) {
-        throw new Error('Error reading Redis config file')
-    }
-
-    logger.info('Loaded redis config from bucket')
-    return parsed
-}
-
-// TODO: remove after upgrading redis in all environments
-// GETEX requires redis version 6.2.0
-function useLegacyOperations(response: string): boolean {
-    const info = response
-        .split('\n')
-        .reduce<Record<string, string>>((acc, line) => {
-            const [key, value] = line.trim().split(':') || []
-            return key && value ? { ...acc, [key]: value } : acc
-        }, {})
-    const version = info['redis_version']
-    logger.info(`Redis server version: ${version}`)
-
-    return version ? semver.lt(version, '6.2.0') : true
 }
 
 async function setupCache(): Promise<void> {
@@ -78,17 +38,14 @@ async function setupCache(): Promise<void> {
 
     logger.info('Loaded redis config', { config })
 
-    const url = config.redisPassword
-        ? `redis://:${config.redisPassword}@${config.redisHost}:${config.redisPort}`
-        : `redis://${config.redisHost}:${config.redisPort}`
+    const url = `redis://:${config.redisPassword}@${config.redisHost}:${config.redisPort}`
 
     const client = createClient({ url })
 
     client.on('error', (err) => logger.error('Redis error caught', err))
 
-    await client.connect().then(() => logger.info('Redis client connected'))
-
-    const useLegacy = useLegacyOperations(await client.info())
+    await client.connect()
+    logger.info('Redis client connected')
 
     setCache = async (
         key: string,
@@ -99,25 +56,11 @@ async function setupCache(): Promise<void> {
     }
 
     getCache = async (key: string, expireInSeconds: number) => {
-        const entry = useLegacy
-            ? await client.get(key).then(async (value) => {
-                  if (value) await client.expire(key, expireInSeconds)
-                  return value
-              })
-            : await client.getEx(key, { EX: expireInSeconds })
+        const entry = await client.getEx(key, { EX: expireInSeconds })
 
         if (entry === null) return null
 
-        const parsed = JSON.parse(entry)
-
-        // TODO: remove after being deployed for some hours
-        // Only used for migrating from HGET/HSET to GET/SET
-        if (parsed.data) {
-            if (setCache) await setCache(key, parsed.data, expireInSeconds)
-            return parsed.data
-        }
-
-        return parsed
+        return JSON.parse(entry)
     }
 }
 
