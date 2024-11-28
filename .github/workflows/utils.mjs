8d53fd50c2dccb6bdd4cdc958bdd1c927123ecd9
tysseng <joakim.tysseng@entur.org>
2024-11-28 14:47:52 +0100

    Nye environments (#253)
    Bytter til dev/tst/prd, endrer credentials for gcp til workload identity federation og tar ibruk ferdige actions for uploading av filter til cloud storage og deploy av app engine app.

---
+++ .github/workflows/utils.mjs
@@ -0,0 +1,47 @@
+const gcpProject = {
+    int: 'ent-enturint-dev',
+    dev: 'ent-enturapp-dev',
+    tst: 'ent-enturapp-tst',
+    beta: 'ent-enturbeta-prd',
+    prd: 'ent-enturapp-prd',
+}
+
+export function calculateEnvironment(inputs) {
+    if (inputs.environment) {
+        return JSON.parse(inputs.environment).map((gcp) => ({
+            gcp,
+            gha: getGhaEnvironment(gcp),
+            gcpProject: gcpProject[gcp],
+        }))
+    }
+
+    return Object.entries({
+        dev: inputs.deployToDev,
+        int: inputs.deployToInt,
+        staging: inputs.deployToStaging,
+        beta: inputs.deployToBeta,
+        prod: inputs.deployToProd,
+    })
+        .map(([env, include]) => (include ? env : undefined))
+        .filter(Boolean)
+        .map((gcp) => ({
+            gcp,
+            gha: getGhaEnvironment(gcp),
+            gcpProject: gcpProject[gcp],
+        }))
+}
+
+// We need to follow the platform naming convention for our GitHub environments to be able to use
+// workload identity federation instead of service account keys for gcp/firebase actions. However,
+// as we have five environments and platform only three, we have to map dev and prod to int and beta
+// (the credentials work across the gcp environments).
+function getGhaEnvironment(gcpEnvironment) {
+    if (gcpEnvironment === 'dev' || gcpEnvironment === 'int') {
+        return 'dev'
+    } else if (gcpEnvironment === 'prod' || gcpEnvironment === 'beta') {
+        return 'prd'
+    } else {
+        // staging
+        return 'tst'
+    }
+}
