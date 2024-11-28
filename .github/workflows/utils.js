73f76388e5184911c188a781cab4f0ceec6c8e44
Joakim Tysseng <joakim.tysseng@entur.org>
2024-11-28 10:10:02 +0100

    Testing workload identity federation

--- .github/workflows/utils.js
+++ .github/workflows/utils.js
@@ -1,6 +1,9 @@
 export function calculateEnvironment(inputs) {
     if (inputs.environment) {
-        return JSON.parse(inputs.environment)
+        return JSON.parse(inputs.environment).map((gcp) => ({
+            gcp,
+            gha: getGhaEnvironment(gcp),
+        }))
     }
 
     return Object.entries({
@@ -12,4 +15,23 @@ export function calculateEnvironment(inputs) {
     })
         .map(([env, include]) => (include ? env : undefined))
         .filter(Boolean)
+        .map((gcp) => ({
+            gcp,
+            gha: getGhaEnvironment(gcp),
+        }))
+}
+
+// We need to follow the platform naming convention for our GitHub environments to be able to use
+// workload identity federation instead of service account keys for gcp/firebase actions. However,
+// as we have five environments and platform only three, we have to map dev and prod to int and beta
+// (the credentials work across the gcp environments).
+function getGhaEnvironment(gcpEnvironment) {
+    if (gcpEnvironment === 'dev' || gcpEnvironment === 'int') {
+        return 'dev'
+    } else if (gcpEnvironment === 'prod' || gcpEnvironment === 'beta') {
+        return 'prd'
+    } else {
+        // staging
+        return 'tst'
+    }
 }
